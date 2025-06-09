694cb7edaf98961a52706e388dc9ed4a4d2f8ccc
Ethan <ebork11@gmail.com>
2025-06-09 14:02:48 -0700

    initial commit

---
+++ src/client/java/borknbeans/savemytools/SaveMyToolsClient.java
@@ -0,0 +1,10 @@
+package borknbeans.savemytools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SaveMyToolsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
